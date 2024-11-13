a1e6efa729f188866eaa1e9c8a610cb8d8405505
Dhanush Rahul Sai <dhanushrahul555@gmail.com>
2024-11-13 17:39:08 -0400

    Create Account complete

--- backend/models/Location.js
+++ backend/models/Location.js
@@ -2,8 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const locationSchema = new Schema({
-    location: { type: String, required: true },
+    location: { type: String, required: true, unique: true },
     passKey: { type: String, required: true },
+    email: { type: String, required: true},
+    name: { type: String, required: true},
     tournaments: [{ type: Schema.Types.ObjectId, ref: 'Tournament' }]
 });
 
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,13 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api', // Replace with your backend URL
+    baseURL: 'http://192.168.2.20:5000/api', // Replace with your backend URL
 });
+
+export const createAccount = async (data) => {
+    return await api.post('/locations/', data);
+};
+
 
 export const getTournaments = async () => {
     const response = await api.get('/tournaments');
